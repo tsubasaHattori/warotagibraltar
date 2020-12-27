931cdbb1c4e39ec53a92fd1cbc2988b90ca28c5c
tsubasaHattori <fourmoonside@gmail.com>
2020-12-28 00:58:40 +0900

    small fix

--- tweet.py
+++ tweet.py
@@ -1,6 +1,8 @@
 from credential import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
 from requests_oauthlib import OAuth1Session
 
+from datetime import datetime
+
 def post_tweet(twitter, body):
     url = "https://api.twitter.com/1.1/statuses/update.json"
     res = twitter.post(url, params={"status": body})
@@ -9,7 +11,7 @@ def post_tweet(twitter, body):
 def main():
     twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
 
-    body = "空爆を要請する！"
+    body = "空爆を要請する！" + str(datetime.now())
     post_tweet(twitter, body)
 
 if __name__ == '__main__':
